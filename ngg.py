24d08e4bf983dc22fdbbf201240300a3274ff11a
Anuj Savant <anujsavant340@gmail.com>
2025-07-06 09:45:49 +0530

    New Development

--- ngg.py
+++ ngg.py
@@ -8,19 +8,15 @@ while num != a:
     if num <1 or num >100:
         print("Invalid number")
         break
+
     elif num < a :
         print("Too low!!")
-        break
+        num = int(input("Enter the number between 1 and 100:"))
+
     elif num > a:
         print("Too high!!")
-        break
-    else:
-        print("Invalid input!")
         num = int(input("Enter the number between 1 and 100:"))
     
-    
-    
-    
-    if num == a:
-        print("You guesses it right!!")
-        
+    elif num == a:
+        print("You guessed right!!")
+        break
